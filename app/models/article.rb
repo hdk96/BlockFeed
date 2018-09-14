c70f3f0d7b5914fa6569a5034fc2f9fa3fe8618d
hdk96 <amir.madi34@gmail.com>
2018-09-14 04:44:33 +0000

    add article and category assosiation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-    
+    has_many :article_categories
+    has_many :categories, through: :article_categories
     validates :title, presence: true, length: { minimum: 10, maximum: 255 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
     validates :link, presence: true, length: { minimum: 5, maximum: 255 }
